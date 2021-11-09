7bf4270d5d860eeec79382c8740e214bbe09ab4e
Owen Alikula <alikulaowen@gmail.com>
2021-11-09 07:47:09 +0300

    Day 0 React and Redux

---
+++ src/state/reducers/index.js
@@ -0,0 +1,12 @@
+import { combineReducers } from "redux"; 
+
+import accountReducer from "./accountReducer";
+
+
+const reducers = combineReducers(
+    {
+        account: accountReducer
+    }
+)
+
+export default reducers;
